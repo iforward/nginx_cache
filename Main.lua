d4526bd90400b2ee8e0f74937802e27eccd6238d
zuan.yu <zuan.yu@vhall.com>
2018-04-26 20:34:22 +0800

    edit

---
+++ Main.lua
@@ -0,0 +1,10 @@
+BASE_PATH = ngx.var.LUA_BASE_PATH;
+package.path = '/usr/local/share/lua/5.1/?.lua;'..BASE_PATH..'/?.lua' 
+
+config = require("conf.Config");
+helpers = require("core.Helpers");
+files = require("core.Files");
+request = require("core.Request");
+cacheDispatch = require("core.cache.Dispatch");
+
+require("Run"):new();
